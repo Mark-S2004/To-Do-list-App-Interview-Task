5ff3d713072f3c9629a6faf2df9a040221d253e3
Mark Saleh <mark-saleh@outlook.com>
2025-01-26 12:40:39 +0200

    ✨ feat: Welcome user name in home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,19 +1,25 @@
 import { TodoForm, TodoList } from "@components/Todo"
+import { useAppSelector } from "@/store"
+import { selectUserName } from "@/store/loggedUserSlice"
 
 const Homepage = () => {
+  const userName = useAppSelector(selectUserName)
   return (
-    <main>
-      <TodoForm />
-      <TodoList
-        todoList={[
-          {
-            title: "Clean car",
-            description: "my car is not clean",
-            dueDate: new Date(2025, 2, 15),
-          },
-        ]}
-      />
-    </main>
+    <>
+      <header>Welcome {userName}</header>
+      <main>
+        <TodoForm />
+        <TodoList
+          todoList={[
+            {
+              title: "Clean car",
+              description: "my car is not clean",
+              dueDate: new Date(2025, 2, 15),
+            },
+          ]}
+        />
+      </main>
+    </>
   )
 }
 
