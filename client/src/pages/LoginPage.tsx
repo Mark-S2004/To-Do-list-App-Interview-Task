ec81dcb1bb525128fa8951b6fb7ffe9c7fe05d44
Mark Saleh <mark-saleh@outlook.com>
2025-01-26 10:11:25 +0200

    ♻️ refactor: put Card component in its own module

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -7,33 +7,13 @@ import FormControl from "@mui/material/FormControl"
 import Link from "@mui/material/Link"
 import TextField from "@mui/material/TextField"
 import Typography from "@mui/material/Typography"
-import MuiCard from "@mui/material/Card"
-import { styled } from "@mui/material/styles"
 import { toast } from "react-toastify"
+import Card from "@components/Card"
 
 import { useLoginUserMutation } from "@/api/authApi"
 import { useNavigate } from "react-router"
 import { FormEvent, useEffect } from "react"
 import { cookieStore } from "@/utils/cookieHash"
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: "auto",
-  [theme.breakpoints.up("sm")]: {
-    maxWidth: "450px",
-  },
-  boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-  ...theme.applyStyles("dark", {
-    boxShadow:
-      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
-  }),
-}))
 
 export default function LoginPage() {
   const navigate = useNavigate()
